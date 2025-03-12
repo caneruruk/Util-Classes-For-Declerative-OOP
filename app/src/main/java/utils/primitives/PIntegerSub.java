05c9a39217cd804da4699145325f2be2f4692b91
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 10:55:11 +0300

    PIntegerSub class added

---
+++ app/src/main/java/utils/primitives/PIntegerSub.java
@@ -0,0 +1,18 @@
+package utils.primitives;
+
+import utils.primitives.interfaces.PInteger;
+
+final public class PIntegerSub implements PInteger {
+    public PIntegerSub(final PInteger left, final PInteger right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int intValue() {
+        return left.intValue() - right.intValue();
+    }
+
+    private final PInteger left;
+    private final PInteger right;
+}
