483a196c7270197f35b7ab8e16ca66220e380c6e
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 07:48:34 +0300

    secondary constructor for int added to PShortAsDouble class

--- app/src/main/java/utils/primitives/PShortAsPDouble.java
+++ app/src/main/java/utils/primitives/PShortAsPDouble.java
@@ -4,6 +4,10 @@ import utils.primitives.interfaces.PDouble;
 import utils.primitives.interfaces.PShort;
 
 final public class PShortAsPDouble implements PDouble {
+    public PShortAsPDouble(final int value) {
+        this(new IntegerAsPShort(value));
+    }
+
     public PShortAsPDouble(final PShort value) {
         this.value = value;
     }
