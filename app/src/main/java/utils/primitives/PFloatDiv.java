46190aa6dabb96f837612d51a5f082f6c26f2259
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 11:44:56 +0300

    PFloatDiv class added

---
+++ app/src/main/java/utils/primitives/PFloatDiv.java
@@ -0,0 +1,18 @@
+package utils.primitives;
+
+import utils.primitives.interfaces.PFloat;
+
+final public class PFloatDiv implements PFloat {
+    public PFloatDiv(final PFloat left, final PFloat right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public float floatValue() {
+        return left.floatValue() / right.floatValue();
+    }
+
+    private final PFloat left;
+    private final PFloat right;
+}
