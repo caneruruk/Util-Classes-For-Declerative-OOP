e9527b290448ad8e19d07f60aa0a4cedac28c548
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 07:52:57 +0300

    secondary constructors added to PShortAsPInteger class

--- app/src/main/java/utils/primitives/PShortAsPInteger.java
+++ app/src/main/java/utils/primitives/PShortAsPInteger.java
@@ -4,6 +4,14 @@ import utils.primitives.interfaces.PInteger;
 import utils.primitives.interfaces.PShort;
 
 final public class PShortAsPInteger implements PInteger {
+    public PShortAsPInteger(final String value) {
+        this(new PStringAsPShort(value));
+    }
+
+    public PShortAsPInteger(final int value) {
+        this(new IntegerAsPShort(value));
+    }
+
     public PShortAsPInteger(final PShort value) {
         this.value = value;
     }
