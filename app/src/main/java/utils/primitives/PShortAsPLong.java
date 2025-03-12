12935a8b93e14dac72c4e7b64c27e57e01f3e635
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 07:53:23 +0300

    secondary constructors added to PShortAsPLong class

--- app/src/main/java/utils/primitives/PShortAsPLong.java
+++ app/src/main/java/utils/primitives/PShortAsPLong.java
@@ -4,6 +4,14 @@ import utils.primitives.interfaces.PLong;
 import utils.primitives.interfaces.PShort;
 
 final public class PShortAsPLong implements PLong {
+    public PShortAsPLong(final String value) {
+        this(new PStringAsPShort(value));
+    }
+
+    public PShortAsPLong(final int value) {
+        this(new IntegerAsPShort(value));
+    }
+
     public PShortAsPLong(final PShort value) {
         this.value = value;
     }
