3c8fbad64588f6ffc566178001e6e62c09b22d77
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 07:58:36 +0300

    secondary constructors added to PIntegerAsPDouble class

--- app/src/main/java/utils/primitives/PIntegerAsPDouble.java
+++ app/src/main/java/utils/primitives/PIntegerAsPDouble.java
@@ -4,6 +4,14 @@ import utils.primitives.interfaces.PDouble;
 import utils.primitives.interfaces.PInteger;
 
 final public class PIntegerAsPDouble implements PDouble {
+    public PIntegerAsPDouble(final String value) {
+        this(new PStringAsPInteger(value));
+    }
+
+    public PIntegerAsPDouble(final int value) {
+        this(new IntegerAsPInteger(value));
+    }
+
     public PIntegerAsPDouble(final PInteger value) {
         this.value = value;
     }
