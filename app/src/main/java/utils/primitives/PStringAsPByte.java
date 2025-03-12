da40bd1be3b3d79727e216872beb3bbaa528a4fb
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 05:54:07 +0300

    secondary ctor for String added to PStringAsPByte class

--- app/src/main/java/utils/primitives/PStringAsPByte.java
+++ app/src/main/java/utils/primitives/PStringAsPByte.java
@@ -4,6 +4,10 @@ import utils.primitives.interfaces.PByte;
 import utils.primitives.interfaces.PString;
 
 final public class PStringAsPByte implements PByte {
+    public PStringAsPByte(final String value) {
+        this(new StringAsPString(value));
+    }
+
     public PStringAsPByte(final PString value) {
         this.value = value;
     }
