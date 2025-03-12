7f21425cdd939cb1d2e064f134ca6aeab01511d3
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 06:02:22 +0300

    secondary ctors added to PByteAsPLong class

--- app/src/main/java/utils/primitives/PByteAsPLong.java
+++ app/src/main/java/utils/primitives/PByteAsPLong.java
@@ -4,6 +4,14 @@ import utils.primitives.interfaces.PByte;
 import utils.primitives.interfaces.PLong;
 
 final public class PByteAsPLong implements PLong {
+    public PByteAsPLong(final String value) {
+        this(new PStringAsPByte(value));
+    }
+
+    public PByteAsPLong(final byte value) {
+        this(new ByteAsPByte(value));
+    }
+
     public PByteAsPLong(final PByte value) {
         this.value = value;
     }
