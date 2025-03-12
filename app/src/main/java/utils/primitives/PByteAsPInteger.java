f3d1142c678a8fd7672faed422b07eb9f02801fc
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 06:00:31 +0300

    secondary ctors added to PByteAsPInteger class

--- app/src/main/java/utils/primitives/PByteAsPInteger.java
+++ app/src/main/java/utils/primitives/PByteAsPInteger.java
@@ -4,6 +4,14 @@ import utils.primitives.interfaces.PByte;
 import utils.primitives.interfaces.PInteger;
 
 final public class PByteAsPInteger implements PInteger {
+    public PByteAsPInteger(final String value) {
+        this(new PStringAsPByte(value));
+    }
+
+    public PByteAsPInteger(final byte value) {
+        this(new ByteAsPByte(value));
+    }
+
     public PByteAsPInteger(final PByte value) {
         this.value = value;
     }
