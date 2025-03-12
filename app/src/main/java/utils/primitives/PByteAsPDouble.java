fbc739807bef5916f50326fb6a6aa4e729844dff
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 05:47:21 +0300

    secondary ctor for byte added to PByteAsPDouble class

--- app/src/main/java/utils/primitives/PByteAsPDouble.java
+++ app/src/main/java/utils/primitives/PByteAsPDouble.java
@@ -4,6 +4,10 @@ import utils.primitives.interfaces.PByte;
 import utils.primitives.interfaces.PDouble;
 
 final public class PByteAsPDouble implements PDouble {
+    public PByteAsPDouble(final byte value) {
+        this(new ByteAsPByte(value));
+    }
+
     public PByteAsPDouble(final PByte value) {
         this.value = value;
     }
