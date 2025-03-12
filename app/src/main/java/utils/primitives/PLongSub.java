588a124b4c14d7d3c41b9de88ff8829f6842b61a
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 10:56:42 +0300

    PLongSub class added

---
+++ app/src/main/java/utils/primitives/PLongSub.java
@@ -0,0 +1,18 @@
+package utils.primitives;
+
+import utils.primitives.interfaces.PLong;
+
+final public class PLongSub implements PLong {
+    public PLongSub(final PLong left, final PLong right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public long longValue() {
+        return left.longValue() - right.longValue();
+    }
+    
+    private final PLong left;
+    private final PLong right;
+}
