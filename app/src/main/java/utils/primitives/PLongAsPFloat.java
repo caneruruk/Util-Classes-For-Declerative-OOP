d246a1996439413409064e46aba2f85a338f27aa
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 08:06:57 +0300

    secondary constructors added to PLongAsPFloat class

--- app/src/main/java/utils/primitives/PLongAsPFloat.java
+++ app/src/main/java/utils/primitives/PLongAsPFloat.java
@@ -4,6 +4,18 @@ import utils.primitives.interfaces.PFloat;
 import utils.primitives.interfaces.PLong;
 
 final public class PLongAsPFloat implements PFloat {
+    public PLongAsPFloat(final String value) {
+        this(new PStringAsPLong(value));
+    }
+
+    public PLongAsPFloat(final long value) {
+        this(new LongAsPLong(value));
+    }
+
+    public PLongAsPFloat(final int value) {
+        this(new PIntegerAsPLong(value));
+    }
+
     public PLongAsPFloat(final PLong value) {
         this.value = value;
     }
