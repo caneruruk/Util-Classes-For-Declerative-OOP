7ccd35d28937703067b16b280d28df5203358453
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 07:59:45 +0300

    secondary constructors added to PIntegerAsPLong class

--- app/src/main/java/utils/primitives/PIntegerAsPLong.java
+++ app/src/main/java/utils/primitives/PIntegerAsPLong.java
@@ -4,6 +4,14 @@ import utils.primitives.interfaces.PInteger;
 import utils.primitives.interfaces.PLong;
 
 final public class PIntegerAsPLong implements PLong {
+    public PIntegerAsPLong(final String value) {
+        this(new PStringAsPInteger(value));
+    }
+
+    public PIntegerAsPLong(final int value) {
+        this(new IntegerAsPInteger(value));
+    }
+    
     public PIntegerAsPLong(final PInteger value) {
         this.value = value;
     }
