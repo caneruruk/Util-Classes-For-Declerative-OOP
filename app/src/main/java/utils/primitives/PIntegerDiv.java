f51d7e0ea224385273020befa554a7135b30e050
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 11:42:29 +0300

    PIntegerDiv class added

---
+++ app/src/main/java/utils/primitives/PIntegerDiv.java
@@ -0,0 +1,18 @@
+package utils.primitives;
+
+import utils.primitives.interfaces.PInteger;
+
+final public class PIntegerDiv implements PInteger {
+    public PIntegerDiv(final PInteger left, final PInteger right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int intValue() {
+        return left.intValue() / right.intValue();
+    }
+
+    private final PInteger left;
+    private final PInteger right;
+}
