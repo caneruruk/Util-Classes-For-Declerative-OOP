712ed0d0456f54c18a34210d8dd69bf95f3d5688
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 05:59:01 +0300

    secondary ctors added to PByteAsPFloat class

--- app/src/main/java/utils/primitives/PByteAsPFloat.java
+++ app/src/main/java/utils/primitives/PByteAsPFloat.java
@@ -4,6 +4,14 @@ import utils.primitives.interfaces.PByte;
 import utils.primitives.interfaces.PFloat;
 
 final public class PByteAsPFloat implements PFloat {
+    public PByteAsPFloat(final String value) {
+        this(new PStringAsPByte(value));
+    }
+
+    public PByteAsPFloat(final byte value) {
+        this(new ByteAsPByte(value));
+    }
+
     public PByteAsPFloat(final PByte value) {
         this.value = value;
     }
