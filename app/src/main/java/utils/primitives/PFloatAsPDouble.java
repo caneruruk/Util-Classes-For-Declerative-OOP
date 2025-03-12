8f9b2a39af6a4ab2c947b917b1da0a53da6ee99f
Caner Ürük <caneruruk5@gmail.com>
2025-03-12 08:11:43 +0300

    secondary constructors added to PFloatAsPDouble class

--- app/src/main/java/utils/primitives/PFloatAsPDouble.java
+++ app/src/main/java/utils/primitives/PFloatAsPDouble.java
@@ -4,6 +4,22 @@ import utils.primitives.interfaces.PDouble;
 import utils.primitives.interfaces.PFloat;
 
 final public class PFloatAsPDouble implements PDouble {
+    public PFloatAsPDouble(final String value) {
+        this(new PStringAsPFloat(value));
+    }
+
+    public PFloatAsPDouble(final float value) {
+        this(new FloatAsPFloat(value));
+    }
+
+    public PFloatAsPDouble(final long value) {
+        this(new PLongAsPFloat(value));
+    }
+
+    public PFloatAsPDouble(final int value) {
+        this(new PIntegerAsPFloat(value));
+    }
+
     public PFloatAsPDouble(final PFloat value) {
         this.value = value;
     }
