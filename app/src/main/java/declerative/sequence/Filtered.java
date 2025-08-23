eaeb0952213f147d70e001cb714668dc292c14b7
Caner Ürük <caneruruk5@gmail.com>
2025-08-23 23:56:32 +0300

    Update Filtered.java

--- app/src/main/java/declerative/sequence/Filtered.java
+++ app/src/main/java/declerative/sequence/Filtered.java
@@ -2,51 +2,56 @@ package declerative.sequence;
 
 import java.util.function.Predicate;
 
+import declerative.primitive.interfaces.PBoolean;
+import declerative.sequence.interfaces.Head;
 import declerative.sequence.interfaces.LazySequence;
+import declerative.sequence.interfaces.Next;
+import declerative.sequence.util.FirstMatch;
+import declerative.sequence.util.HasMatch;
 
 final public class Filtered<T> implements LazySequence<T> {
     public Filtered(final LazySequence<T> lazySequence, final Predicate<T> predicate) {
-        this.lazySequence = lazySequence;
-        this.predicate = predicate;
+        this(
+            new HasMatch<>(lazySequence, predicate),
+            new Next<T>() {
+                @Override
+                public LazySequence<T> asLazySequence() {
+                    LazySequence<T> tempLazySequence = lazySequence;
+                    while(tempLazySequence.hasHead()) {
+                        if(predicate.test(tempLazySequence.head()))
+                            return new Filtered<>(tempLazySequence.next(), predicate);
+                        tempLazySequence = tempLazySequence.next();
+                    }
+
+                    return new Filtered<>(lazySequence, predicate);
+                }
+            },
+            new FirstMatch<>(lazySequence, predicate)
+        );
+    }
+
+    public Filtered(final PBoolean hasHead, final Next<T> next, final Head<T> head) {
+        this.hasHead = hasHead;
+        this.next = next;
+        this.head = head;
     }
 
     @Override
     public boolean hasHead() {
-        LazySequence<T> tempLazySequence = lazySequence;
-        while(tempLazySequence.hasHead()) {
-            if(predicate.test(tempLazySequence.head()))
-                return true;
-            tempLazySequence = tempLazySequence.next();
-        }
-
-        return false;
+        return hasHead.boolValue();
     }
 
     @Override
     public LazySequence<T> next() {
-        LazySequence<T> tempLazySequence = lazySequence;
-        while(tempLazySequence.hasHead()) {
-            if(predicate.test(tempLazySequence.head()))
-                return new Filtered<>(tempLazySequence.next(), predicate);
-            tempLazySequence = tempLazySequence.next();
-        }
-
-        return new Filtered<>(lazySequence, predicate);
+        return next.asLazySequence();
     }
 
     @Override
     public T head() {
-        LazySequence<T> tempLazySequence = lazySequence;
-        while(tempLazySequence.hasHead()) {
-            final T currentItem = tempLazySequence.head();
-            if(predicate.test(currentItem))
-                return currentItem;
-            tempLazySequence = tempLazySequence.next();
-        }
-
-        return lazySequence.head();
+        return head.value();
     }
 
-    private final LazySequence<T> lazySequence;
-    private final Predicate<T> predicate;
+    private final PBoolean hasHead;
+    private final Next<T> next;
+    private final Head<T> head;
 }
