5cee4fbb27607e6f69cca63f9eb7efdb01768884
Caner Ürük <caneruruk5@gmail.com>
2025-08-24 00:48:58 +0300

    Update Taken.java

--- app/src/main/java/declerative/sequence/Taken.java
+++ app/src/main/java/declerative/sequence/Taken.java
@@ -1,37 +1,59 @@
 package declerative.sequence;
 
+import declerative.primitive.arithmetic.PBooleanAnd;
 import declerative.primitive.arithmetic.PLongSub;
+import declerative.primitive.comparison.PLongIsMoreThan;
 import declerative.primitive.conversion.LongAsPLong;
+import declerative.primitive.interfaces.PBoolean;
 import declerative.primitive.interfaces.PLong;
+import declerative.sequence.interfaces.Head;
 import declerative.sequence.interfaces.LazySequence;
+import declerative.sequence.interfaces.Next;
+import declerative.sequence.util.HasHead;
+import declerative.sequence.util.LazySequenceHead;
 
 final public class Taken<T> implements LazySequence<T> {
     public Taken(final LazySequence<T> lazySequence, final PLong target) {
-        this(lazySequence, target, new PLongSub(target, new LongAsPLong(1)));
+        this(
+            new PBooleanAnd(
+                new PLongIsMoreThan(
+                    target,
+                    new LongAsPLong(0)
+                ),
+                new HasHead<>(lazySequence)
+            ),
+            new Next<T>() {
+                @Override
+                public LazySequence<T> asLazySequence() {
+                    return new Taken<T>(lazySequence.next(), new PLongSub(target, new LongAsPLong(1)));
+                }
+            },
+            new LazySequenceHead<>(lazySequence)
+        );
     }
 
-    public Taken(final LazySequence<T> lazySequence, final PLong target, final PLong nextTarget) {
-        this.lazySequence = lazySequence;
-        this.target = target;
-        this.nextTarget = nextTarget;
+    public Taken(final PBoolean hasHead, final Next<T> next, final Head<T> head) {
+        this.hasHead = hasHead;
+        this.next = next;
+        this.head = head;
     }
 
     @Override
     public boolean hasHead() {
-        return target.longValue() > 0 && lazySequence.hasHead();
+        return hasHead.boolValue();
     }
 
     @Override
     public LazySequence<T> next() {
-        return new Taken<>(lazySequence.next(), nextTarget);
+        return next.asLazySequence();
     }
 
     @Override
     public T head() {
-        return lazySequence.head();
+        return head.value();
     }
 
-    private final LazySequence<T> lazySequence;
-    private final PLong target;
-    private final PLong nextTarget;
+    private final PBoolean hasHead;
+    private final Next<T> next;
+    private final Head<T> head;
 }
