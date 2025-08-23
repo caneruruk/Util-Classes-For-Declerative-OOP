743dc281a543d196c9e928f63c88143c95db699a
Caner Ürük <caneruruk5@gmail.com>
2025-08-24 01:00:10 +0300

    Update Range.java

--- app/src/main/java/declerative/sequence/Range.java
+++ app/src/main/java/declerative/sequence/Range.java
@@ -7,29 +7,36 @@ import declerative.primitive.comparison.PLongIsMoreThan;
 import declerative.primitive.conversion.LongAsPLong;
 import declerative.primitive.interfaces.PBoolean;
 import declerative.primitive.interfaces.PLong;
+import declerative.sequence.interfaces.Head;
 import declerative.sequence.interfaces.LazySequence;
+import declerative.sequence.interfaces.Next;
 
 final public class Range implements LazySequence<Long> {
     public Range(final PLong source, final PLong target, final PLong step) {
         this(
-            new PLongAdd(source, step),
             new PBooleanChoice(
                 new PLongIsMoreThan(step, new LongAsPLong(0)), 
                 new PLongIsLessThan(source, target), 
                 new PLongIsMoreThan(source, target)
             ),
-            source,
-            target,
-            step
+            new Next<Long>() {
+                @Override
+                public LazySequence<Long> asLazySequence() {
+                    return new Range(new PLongAdd(source, step), target, step);
+                }
+            },
+            new Head<Long>() {
+                public Long value() {
+                    return source.longValue();
+                }
+            }
         );
     }
 
-    public Range(final PLong nextSource, final PBoolean hasHead, final PLong head, final PLong target, final PLong step) {
-        this.nextSource = nextSource;
+    public Range(final PBoolean hasHead, final Next<Long> next, final Head<Long> head) {
         this.hasHead = hasHead;
+        this.next = next;
         this.head = head;
-        this.target = target;
-        this.step = step;
     }
 
     @Override
@@ -39,17 +46,15 @@ final public class Range implements LazySequence<Long> {
 
     @Override
     public LazySequence<Long> next() {
-        return new Range(nextSource, target, step);
+        return next.asLazySequence();
     }
 
     @Override
     public Long head() {
-        return head.longValue();
+        return head.value();
     }
 
-    private final PLong nextSource;
     private final PBoolean hasHead;
-    private final PLong head;
-    private final PLong target;
-    private final PLong step;
+    private final Next<Long> next;
+    private final Head<Long> head;
 }
